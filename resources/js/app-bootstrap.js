0f2b5d6d537b0d44b76c3ae3e6a4cc684d9ef7d5
Raviraj Chauhan <rjchauhan8427@gmail.com>
2020-01-17 19:50:17 +0530

    removed axios setup

--- resources/js/app-bootstrap.js
+++ resources/js/app-bootstrap.js
@@ -2,7 +2,6 @@
  * Load various JavaScript modules that assist App.
  */
 window.URI = require('urijs');
-window.axios = require('axios');
 window._ = require('lodash');
 window.moment = require('moment');
 window.Promise = require('promise');
@@ -46,17 +45,5 @@ require('bootstrap');
 if ($('#app').length > 0) {
     require('vue-bootstrap');
 }
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-window.axios.defaults.headers.common = {
-    'X-Requested-With': 'XMLHttpRequest',
-    'X-CSRF-TOKEN': App.csrfToken
-};
-
-require('./interceptors');
 
 require('./helpers');
